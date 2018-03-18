9a2063cb38b3285e1c1861e192d4c0fac529d112
Rob <brucer@702ubuSvr.hsd1.or.comcast.net>
2018-03-17 20:53:22 -0700

    initial commit

---
+++ ccminer.template.nox16r/rvn-suprnova.sh
@@ -0,0 +1,1 @@
+./ccminer/ccminer -d 0,1,2,3,4 -a x16r -o stratum+tcp://rvn.suprnova.cc:6667 -u entegral.702ubu -p x
---
+++ ccminer.template.nox16r/rvn.all.sh
@@ -0,0 +1,1 @@
+./ccminer/ccminer -a x16r -o stratum+tcp://rvn.suprnova.cc:6667 -u entegral.702ubuSvr -p x
