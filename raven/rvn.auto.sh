7392aaf9ef1fba1567dae5e65c39e69a288582a8
Rob <brucer@702ubuSvr.hsd1.or.comcast.net>
2018-03-19 12:43:11 -0700

    updates

--- raven/rvn.auto.sh
+++
@@ -1,1 +0,0 @@
-./ccminer/ccminer -d 1,2,3,4,5,6,7,8 -a x16r -i 18.5 -o stratum+tcp://rvn.suprnova.cc:6667 -u entegral.702ubu -p x
---
+++ raven/rvn.b.sh
@@ -0,0 +1,1 @@
+./ccminer/ccminer -d 4,5,6,7,8 -a x16r -i 19 -o stratum+tcp://rvn.suprnova.cc:6667 -u entegral.702ubu -p x
